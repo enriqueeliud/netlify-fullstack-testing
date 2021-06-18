73ec2c8948030442ee309222bf98c5e0de42d7a5
Eliud Maina <chiuri@uoemdomain.embuni.ac.ke>
2021-06-18 13:47:19 +0300

    initial commit

--- monjo/src/App.js
+++ monjo/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import mine from './components/Mine'
 
 function App() {
   return (
     <div className="App">
-     <h1>ghhhhhhhbn </h1>
+     <mine />
     </div>
   );
 }
---
+++ monjo/src/components/Mine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mine() {
+ return (
+  <div>
+   <h1>jfrnrtjkvbrt</h1>
+  </div>
+ )
+}
+
+export default Mine
